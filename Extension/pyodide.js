0a60e3ec97f007bce8d95563044d900c54581203
Zenr0y <66503573+Zenr0y@users.noreply.github.com>
2025-02-02 10:29:01 -0500

    Delete Extension/pyodide.js

--- Extension/pyodide.js
+++
@@ -1,7 +0,0 @@
-async function loadPyodide() {
-    if (!window.pyodide) {
-        window.pyodide = await import("https://cdn.jsdelivr.net/pyodide/v0.23.4/full/pyodide.mjs");
-        window.pyodide = await window.pyodide.loadPyodide();
-    }
-    return window.pyodide;
-}
